bb415e2055e0528376454da05224286859f37150
ygz <ronin_ygz@163.com>
2026-09-11 10:22:15 +0800

    first commit

---
+++ testSdk/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.maven.library)
+}
+
+android {
+    namespace = "com.example.testsdk"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.yangzi"
+            artifactId = "testSdk"
+            version = "1.0.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+
+            url = uri(
+                "https://github.com/yandaxianer-droid/testSdk"
+            )
+
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
---
+++ testSdk/src/main/java/com/example/testsdk/TestSdk.kt
@@ -0,0 +1,12 @@
+package com.example.testsdk
+
+import android.content.Context
+import android.widget.Toast
+
+class TestSdk {
+
+    fun toastHHH(context: Context){
+        Toast.makeText(context,"HHH", Toast.LENGTH_SHORT).show()
+    }
+
+}
