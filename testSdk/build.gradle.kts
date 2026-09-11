96104621f1a4a770a9b8957661913d3d0907fd9d
ygz <ronin_ygz@163.com>
2026-09-11 10:46:44 +0800

    1.0.2

--- testSdk/build.gradle.kts
+++ testSdk/build.gradle.kts
@@ -51,7 +51,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.yangzi"
             artifactId = "testSdk"
-            version = "1.0.0"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
--- testSdk/src/main/java/com/example/testsdk/TestSdk.kt
+++ testSdk/src/main/java/com/example/testsdk/TestSdk.kt
@@ -3,7 +3,7 @@ package com.example.testsdk
 import android.content.Context
 import android.widget.Toast
 
-class TestSdk {
+object TestSdk {
 
     fun toastHHH(context: Context){
         Toast.makeText(context,"HHH", Toast.LENGTH_SHORT).show()
